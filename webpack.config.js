8aa93ddc3d96c9f11b14160894107752b356b083
maitiantian <876633545@qq.com>
2018-11-23 13:45:43 +0800

    add webpack HMR

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,5 @@
-const path = require('path')
 const HtmlWebPackPlugin = require('html-webpack-plugin') // 用于导入在内存中自动生成的index页面的插件
-
-// 创建插件的实例对象
-const htmlPlugin = new HtmlWebPackPlugin({
-    template: path.join(__dirname, './src/index.html'), // 源文件
-    filename: 'index.html' // 生成的内存中首页的名称
-})
-
+const webpack = require('webpack');
 
 // 因为webpack是基于Node构建的，所以webpack支持所有Node API和语法；
 // Node使用Chrome 的V8 JavaScript引擎来运行代码，所以Chrome浏览器支持哪些新的JS特性，则Node就支持哪些。
@@ -14,9 +7,27 @@ const htmlPlugin = new HtmlWebPackPlugin({
 // 向外暴露一个打包的配置对象；
 module.exports = {
     mode: 'development', // development | production
+
     // 在webpack 4.x中，有一个很大的特性，就是“约定大于配置”，默认的打包入口路径是src -> index.js
+    entry: `${__dirname}/src/index.js`,
+    output: {
+        filename: 'bundle.js',
+        path: `${__dirname}/dist/static/`
+    },
+
+    devServer: {
+        contentBase: './dist',
+        port: 8000,
+        hotOnly: true
+    },
+
     plugins: [
-        htmlPlugin
+        // 创建插件的实例对象
+        new HtmlWebPackPlugin({
+            template: `${__dirname}/src/index.html`, // 源文件
+            filename: 'index.html' // 生成的内存中首页的名称
+        }),
+        new webpack.HotModuleReplacementPlugin()
     ],
     // webpack默认只能打包处理.js后缀名类型的文件，像.png、.vue无法主动处理，所以要配置第三方的loader。
     module: {       // 所有第三方模块的配置规则
